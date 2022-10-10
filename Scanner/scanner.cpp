aca1521ef2b0d6255f241331cdee9a50617a045b
Raymond Quach <56996265+dj-shoko@users.noreply.github.com>
2022-10-10 11:59:30 -0700

    Improved Word function efficiency
    "State 7 block" removed, it only existed to prevent words like "ann" from passing, more efficient way implemented in line 90 by just making sure there is never an "nn" to indicate an ending of a valid string.

--- Scanner/scanner.cpp
+++ Scanner/scanner.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 // --------- Two DFAs ---------------------------------
 
-// WORD DFA 
+// WORD DFA
 // Done by: Raymond Quach
 // RE:   **
 
@@ -23,14 +23,14 @@ bool word (string s)
   int charpos = 0; // Initialize the character position as the first letter in string
 
   // Loop to go through each letter in the string
-  while (s[charpos] != '\0') 
+  while (s[charpos] != '\0')
     {
       // State 0 block
       // Check if it is a vowel state
       if (state == 0 && (s[charpos] == 'a' || tolower(s[charpos]) == 'e' ||
       tolower(s[charpos]) == 'i' || s[charpos] == 'o' || s[charpos] == 'u'))
         state = 1;
-      
+
       // Consanant check, non-pair state
       else if (state == 0 && (s[charpos] == 'd' || s[charpos] == 'j' ||
       s[charpos] == 'w' || s[charpos] == 'y' || s[charpos] == 'z'))
@@ -45,11 +45,11 @@ bool word (string s)
       // Special consanant in case of "ch" state
       else if (state == 0 && s[charpos] == 'c')
         state = 4;
-      
+
       // Consanant 's' case this is a consonant pair state
       else if (state == 0 && s[charpos] == 's')
         state = 5;
-        
+
       // Consanant 't' case this is a consonant pair state
       else if (state == 0 && s[charpos] == 't')
         state = 6;
@@ -62,7 +62,7 @@ bool word (string s)
       else if (state == 1 && (s[charpos] == 'a' || tolower(s[charpos]) == 'e' ||
       tolower(s[charpos]) == 'i' || s[charpos] == 'o' || s[charpos] == 'u'))
         state = 1;
-      
+
       // Consanant check, non-pair state (qsa)
       else if (state == 1 && (s[charpos] == 'd' || s[charpos] == 'j' ||
       s[charpos] == 'w' || s[charpos] == 'y' || s[charpos] == 'z'))
@@ -77,18 +77,18 @@ bool word (string s)
       // Special consanant in case of "ch" state
       else if (state == 1 && s[charpos] == 'c')
         state = 4;
-      
+
       // Consanant 's' case this is a consonant pair state
       else if (state == 1 && s[charpos] == 's')
         state = 5;
-        
+
       // Consanant 't' case this is a consonant pair state
       else if (state == 1 && s[charpos] == 't')
         state = 6;
 
       // Is it a vowel followed up by an n
-      else if (state == 1 && s[charpos] == 'n')
-        state = 7;
+      else if (state == 1 && s[charpos] == 'n' && s[charpos-1] != 'n')
+        state = 1;
 
       // Consonant Pair State Block
       else if (state == 3 && s[charpos] == 'y' ||
@@ -109,53 +109,23 @@ bool word (string s)
 
 
 
-      // State 7 block
-      // Check if it is a vowel state
-      else if (state == 7 && (s[charpos] == 'a' || tolower(s[charpos]) == 'e' ||
-      tolower(s[charpos]) == 'i' || s[charpos] == 'o' || s[charpos] == 'u'))
-        state = 1;
-      
-      // Consanant check, non-pair state (qsa)
-      else if (state == 7 && (s[charpos] == 'd' || s[charpos] == 'j' ||
-      s[charpos] == 'w' || s[charpos] == 'y' || s[charpos] == 'z'))
-        state = 2;
-
-      // Consanant check, pair allowed state
-      else if (state == 7 && (s[charpos] == 'b' || s[charpos] == 'g' ||
-      s[charpos] == 'h' || s[charpos] == 'k' || s[charpos] == 'm' ||
-      s[charpos] == 'p' || s[charpos] == 'r'))
-        state = 3;
-
-      // Special consanant in case of "ch" state
-      else if (state == 7 && s[charpos] == 'c')
-        state = 4;
-      
-      // Consanant 's' case this is a consonant pair state
-      else if (state == 7 && s[charpos] == 's')
-        state = 5;
-        
-      // Consanant 't' case this is a consonant pair state
-      else if (state == 7 && s[charpos] == 't')
-        state = 6;
-      // End of state 7 block
-
       else {
         // Debugging
         // if (s != ".")
-        //  cout << "DEBUG:\nString: " << s << ", Char: " << s[charpos] << charpos << endl;
+        //  cout << "DEBUG:\n\tString: " << s << ", Char: " << s[charpos] << charpos << endl;
 	      return(false);
       }
       charpos++;
     }
 
   // where did I end up????
-  if (state == 1 || state == 7)
-    return(true);  // end in a final state
+  if (state == 1) // end in a final state
+    return(true);
   else
     return(false);
 }
 
-// PERIOD DFA 
+// PERIOD DFA
 // Done by: Raymond Quach
 bool period (string s)
 {
@@ -171,34 +141,35 @@ bool period (string s)
 
 // TABLES Done by: Raymond Quach
 
-// ** Update the tokentype to be WORD1, WORD2, PERIOD, ERROR, EOFM, etc.
+// Table for the token type.
 enum tokentype {
 VERB, VERBNEG, VERBPAST, VERBPASTNEG, IS, WAS,
 OBJECT, SUBJECT, DESTINATION, PRONOUN, CONNECTOR,
 WORD1, WORD2, PERIOD, ERROR,
 EOFM};
 
-// ** For the display names of tokens - must be in the same order as the tokentype.
+// String table for the display names of tokens, same order as "tokentype" table
 string tokenName[30] = {
 "VERB", "VERBNEG", "VERBPAST", "VERBPASTNEG", "IS", "WAS",
 "OBJECT", "SUBJECT", "DESTINATION", "PRONOUN", "CONNECTOR",
 "WORD1", "WORD2", "PERIOD", "ERROR",
 "EOFM"
-}; 
+};
 
-// ** Need the reservedwords table to be set up here. 
+// ** Need the reservedwords table to be set up here.
 // ** Do not require any file input for this. Hard code the table.
 // ** a.out should work without any additional files.
 
+// String table for reserved words
 string reservedwords[30] = {
   "masu", "masen", "mashita", "masendeshita", "desu", "deshita", //Verbs
   "o", "wa", "ni", //Particles
   "watashi", "anata", "kare", "kanojo", "sore", //Pronouns
   "mata", "soshite", "shikashi", "dakara", //Connector
   "eofm" //End of File Marker
-}; 
+};
 
-// ------------ Scanner and Driver ----------------------- 
+// ------------ Scanner and Driver -----------------------
 
 ifstream fin;  // global stream for reading from the input file
 
@@ -206,7 +177,7 @@ ifstream fin;  // global stream for reading from the input file
 // Gives back the token type and the word itself
 // ** Done by: Raymond Quach
 int scanner(tokentype& tt, string& w)
-{ 
+{
   // Grab the next word from the file via fin
   fin >> w;
 
@@ -288,21 +259,21 @@ int scanner(tokentype& tt, string& w)
     else if (w[finalChar] == 'E' || w[finalChar] == 'I')
       tt = WORD2;
   }
-  
+
   else // If it is neither a word nor a period, then it is an error
     tt = ERROR;
 } //the end of scanner
 
 
 
-// The temporary test driver to just call the scanner repeatedly  
+// The temporary test driver to just call the scanner repeatedly
 // This will go away after this assignment
-// DO NOT CHANGE THIS!!!!!! 
+// DO NOT CHANGE THIS!!!!!!
 // Done by:  Louis
 int main()
 {
   tokentype thetype;
-  string theword; 
+  string theword;
   string filename;
 
   cout << "Enter the input file name: ";
@@ -314,11 +285,11 @@ int main()
    while (true)
     {
        scanner(thetype, theword);  // call the scanner which sets
-                                   // the arguments  
+                                   // the arguments
        if (theword == "eofm") break;  // stop now
 
        cout << "Type is:" << tokenName[thetype] << endl;
-       cout << "Word is:" << theword << endl;   
+       cout << "Word is:" << theword << endl;
     }
 
    cout << "End of file is encountered." << endl;
